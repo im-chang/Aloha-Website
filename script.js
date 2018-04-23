0420ff08d81ebc192acaae543396fac96c0c4033
im-chang <38142960+im-chang@users.noreply.github.com>
2018-04-22 23:53:59 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+    // $('.main-carousel').flickity({
+    //     // options
+    //     cellAlign: 'left',
+    //     contain: true
+    //   });
+
+}); // end of document.ready()
